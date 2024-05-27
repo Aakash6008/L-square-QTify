5e193da8322278a929a8f260a27c9d72578f1957
Aakash6008 <aakashpokhriyal888@gmail.com>
2024-05-27 22:05:42 +0530

    added Hero-Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,14 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero-section/Hero';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import logo from "./image.png"
-import SearchBar from '../SearchBar/SearchBar';
+import SearchIcon from "@mui/icons-material/Search";
 
 const Navbar = () => {
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <div className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="logo" className="logo-image" />
       </div>
-      <SearchBar/>
+      <div className="navbar-search">
+      <div className="searchbar-input">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search an album of your choice"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+      <button className="search-button">
+          <SearchIcon />
+        </button>
+    </div>
       <button className="feedback-button">Give Feedback</button>
     </div>
   );
